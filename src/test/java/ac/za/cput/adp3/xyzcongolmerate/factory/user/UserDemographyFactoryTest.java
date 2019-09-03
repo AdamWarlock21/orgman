ada5b2d3361058a76d95a8546e056439e2bc4bf8
AdamWarlock21 <47519717+AdamWarlock21@users.noreply.github.com>
2019-09-03 17:06:18 +0200

    Update UserDemographyFactoryTest.java

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
@@ -3,12 +3,13 @@ package ac.za.cput.adp3.xyzcongolmerate.factory.user;
 import ac.za.cput.adp3.xyzcongolmerate.domain.user.UserDemography;
 import org.junit.Assert;
 import org.junit.Test;
-
+import java.util.Date;
 
 public class UserDemographyFactoryTest {
 
     @Test
     public void buildUserDemography() {
+        Date date = new Date();
         UserDemography userDemography = UserDemographyFactory.buildUserDemography("bleeep@mycput.ac.za", "Admin", "gender", "race", date);
         System.out.println("The User Demography Email is: " + userDemography.getUserEmail());
         Assert.assertNotNull(userDemography);
